979e859a0edd827655bf3eb815cf7b7714c2453b
Alexandru Miclea <alexandrumiclea@outlook.com>
2024-11-27 23:45:06 +0200

    Optimized exercises

--- Lab8/ex1.py
+++ Lab8/ex1.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
+from matplotlib.backends.backend_pdf import PdfPages
 
 N = 1000
+pp = PdfPages('plots/Exercitiul_1.pdf')
 
 # a
+
+fig, axs = plt.subplots(4)
+plt.title('Serie timp')
 
 ec_grad2  = lambda x: x**2
 spatiu_semnal = np.linspace(0,1,N)
@@ -17,26 +22,75 @@ spatiu_semnal = semnal(3,6)
 
 noise = np.random.random(size=N) / 10
 
+axs[0].plot(spatiu_serie_timp)
+axs[0].set_title('Ecuatie grad 2')
 spatiu_serie_timp = np.add(spatiu_serie_timp, noise)
+axs[1].plot(noise)
+axs[1].set_title('Zgomot')
 spatiu_serie_timp += np.add(spatiu_serie_timp, spatiu_semnal)
-plt.title('Serie timp')
+axs[2].plot(spatiu_semnal)
+axs[2].set_title('Semnal')
+
 plt.xlabel("Ordin esantion")
 plt.ylabel("Valoare")
 plt.plot(spatiu_serie_timp)
 plt.show()
 
+pp.savefig()
+plt.savefig("plots/Exercitiul_1a.svg", format='svg')
+plt.savefig("plots/Exercitiul_1a.png", format='png')
+
 # b
 
-# todo trebuie refacut
+def self_autocorrelation(timeseries):
+    
+    N = timeseries.shape[0]
+    
+    ret_values = np.zeros(2 * N - 1)
+    
+    for idx in range(2 * N - 1):
+        
+        if (idx < N):
+            idx1_left = N - 1 - idx
+            idx1_right = N
+            idx2_left = 0
+            idx2_right = idx + 1
+            
+        else:
+            idx1_left = 0
+            idx1_right = 2 * N - idx
+            idx2_left = idx - N
+            idx2_right = N
+            
+        sub_1 = timeseries[idx1_left:idx1_right]
+        sub_2 = timeseries[idx2_left:idx2_right]
+        
+        ret_values[idx] = np.dot(sub_1, sub_2)
+    
+    return ret_values
+
 
 test = np.correlate(spatiu_serie_timp, spatiu_serie_timp, "full")
+test2 = self_autocorrelation(spatiu_serie_timp)
 
 #print(np.sum(spatiu_serie_timp))
 
-plt.figure()
-plt.title('B')
-plt.plot(test / np.sum(spatiu_serie_timp**2))
+fig, axs = plt.subplots(2)
+#plt.title('Self-autocorrelation vs NumPy')
+axs[0].plot(test / np.sum(spatiu_serie_timp**2))
+axs[0].set_title("NumPy")
+axs[1].plot(test2 / np.sum(spatiu_serie_timp**2))
+axs[1].set_title("Autocorelare implementata")
 plt.show()
+
+pp.savefig()
+plt.savefig("plots/Exercitiul_1b.svg", format='svg')
+plt.savefig("plots/Exercitiul_1b.png", format='png')
+
+norm_test = np.linalg.norm(test)
+norm_test2 = np.linalg.norm(test2)
+
+print (f"It is {np.abs(norm_test - norm_test2) <= 1e6} that my autocorrelation is the best!")
 
 # c
 
@@ -73,6 +127,10 @@ plt.plot(spatiu_predictii, predictii, label=f'Valori prezise pentru p = {p}')
 plt.axvline(p, label = 'Numarul predictiilor luate in model', color = 'red')
 plt.legend()
 plt.show()
+
+pp.savefig()
+plt.savefig("plots/Exercitiul_1c.svg", format='svg')
+plt.savefig("plots/Exercitiul_1c.png", format='png')
 
 # d
 
@@ -125,3 +183,9 @@ m_slider.on_changed(update_predictions)
 
 fig.legend()
 fig.show()
+
+pp.savefig()
+plt.savefig("plots/Exercitiul_1d.svg", format='svg')
+plt.savefig("plots/Exercitiul_1d.png", format='png')
+
+pp.close()
